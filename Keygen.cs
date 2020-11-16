dc7b7183d1b1ec7c16d8998cf3ea676376c77d35
Amrit Panesar <me@amrit.pw>
2020-11-16 11:46:52 -0800

    Initial working code, version 1.0.0

---
+++ Keygen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace mononucleosis_pro
+{
+    class Keygen
+    {
+        public const int SUBLIME_TEXT = 0;
+        public const int VISUAL_STUDIO_CODE = 1;
+        private static readonly byte[] VSCGUIDBytes = new byte[] { 111, 15, 51, 253, 65, 63, 29, 66, 159, 229, 222, 116, 45, 12, 84, 192 };
+        private static readonly MD5 MD5 = MD5.Create();
+        private static readonly Guid VSCGUID = new Guid(VSCGUIDBytes);
+
+        public string Generate(int type, string email)
+        {
+            switch (type)
+            {
+                case SUBLIME_TEXT:
+                    return generateSublime(email);
+
+                case VISUAL_STUDIO_CODE:
+                    return generateVSCode(email);
+
+                default:
+                    return "";
+            }
+        }
+
+        private string generateSublime(string input)
+        {
+            string output = BitConverter.ToString(MD5.ComputeHash(Encoding.UTF8.GetBytes(input))).Replace("-", "").Substring(0, 25).ToLower();
+
+            return licenseFormat(output);
+        }
+
+        private string generateVSCode(string input)
+        {
+            input = VSCGUID.ToString() + input;
+
+            string output = BitConverter.ToString(MD5.ComputeHash(Encoding.UTF8.GetBytes(input))).Replace("-", "").Substring(0, 25).ToLower();
+
+            return licenseFormat(output);
+        }
+
+        private string licenseFormat(string input)
+        {
+            string output = "";
+
+            for (int i = 0; i <= input.Length + 1; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    if (i + 5 < input.Length)
+                    {
+                        output += input.Substring(i, 5) + "-";
+                    }
+                    else
+                    {
+                        output += input.Substring(i);
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mononucleosis_pro
+{
+    public partial class Main : Form
+    {
+        Keygen keygen = new Keygen();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.cbxVersion.SelectedIndex = Keygen.SUBLIME_TEXT;
+            btnGenerate.PerformClick();
+        }
+
+        private void txtEmail_Enter(object sender, EventArgs e)
+        {
+            txtEmail.SelectAll();
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+            txtEmail.SelectAll();
+        }
+
+        private void txtLicense_Click(object sender, EventArgs e)
+        {
+            txtLicense.SelectAll();
+        }
+
+        private void cbxVersion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            generate();
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            generate();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            generate();
+            txtLicense.SelectAll();
+        }
+
+        private void generate()
+        {
+            txtLicense.Text = keygen.Generate(cbxVersion.SelectedIndex, txtEmail.Text);
+        }
+
+    }
+}
