1471136799a7a543c51cb77042ed9fe2ae774485
Neo <321592+Neo-Desktop@users.noreply.github.com>
2020-11-16 11:46:52 -0800

    Initial working code, version 1.0.0

---
+++ Main.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace mononucleosis_pro
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.lblLicense = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.cbxVersion = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGenerate.Location = new System.Drawing.Point(104, 146);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(76, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtEmail.Location = new System.Drawing.Point(12, 72);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(260, 23);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.Text = "me@example.com";
+            this.txtEmail.Click += new System.EventHandler(this.txtEmail_Click);
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            this.txtEmail.Enter += new System.EventHandler(this.txtEmail_Enter);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 54);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(36, 15);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtLicense.Location = new System.Drawing.Point(12, 116);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.ReadOnly = true;
+            this.txtLicense.Size = new System.Drawing.Size(260, 23);
+            this.txtLicense.TabIndex = 3;
+            this.txtLicense.Click += new System.EventHandler(this.txtLicense_Click);
+            // 
+            // lblLicense
+            // 
+            this.lblLicense.AutoSize = true;
+            this.lblLicense.Location = new System.Drawing.Point(12, 98);
+            this.lblLicense.Name = "lblLicense";
+            this.lblLicense.Size = new System.Drawing.Size(46, 15);
+            this.lblLicense.TabIndex = 6;
+            this.lblLicense.Text = "License";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(12, 9);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(45, 15);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = "Version";
+            // 
+            // cbxVersion
+            // 
+            this.cbxVersion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbxVersion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxVersion.FormattingEnabled = true;
+            this.cbxVersion.Items.AddRange(new object[] {
+            "Sublime Text 3",
+            "Visual Studio Code"});
+            this.cbxVersion.Location = new System.Drawing.Point(13, 27);
+            this.cbxVersion.Name = "cbxVersion";
+            this.cbxVersion.Size = new System.Drawing.Size(259, 23);
+            this.cbxVersion.TabIndex = 1;
+            this.cbxVersion.SelectedIndexChanged += new System.EventHandler(this.cbxVersion_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 181);
+            this.Controls.Add(this.cbxVersion);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblLicense);
+            this.Controls.Add(this.txtLicense);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mononucleosis Pro";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Label lblLicense;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.ComboBox cbxVersion;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mononucleosis-pro
 Generates keys for an aptly named **color scheme**?
+
+![logo, f in chat](img/logo.png)
+
+`Press F to pay respects`
+
+## Instructions
+
+### Sublime Text 3
+* Open *Sublime Text 3* and ensure you have `Theme - <package name>` installed
+* Open up the user preferences for the theme `(Preferences > Package Settings > Theme - <package name> > Settings - User)`
+* Copy the following text into the file, with the placeholder values changed appropriately
+
+```json
+{
+    "email": "{add your email here}",
+    "license_key": "{add your license key here}"
+}
+```
+
+* Save the file and restart *Sublime Text 3*
+* You should see a thank you pop-up message and you are now running an activated theme.
+
+
+![sublime text 3](img/st3.png)
+
+### Visual Studio Code
+* Open *Visual Studio Code* and ensure you have `<package name>` installed.
+* Press `Ctrl+Shift+P` to open the command palette.
+* Search for `<package name>` and select the option `<package name> - enter license.`
+* Follow the prompts on-screen to enter your email and license key.
+* You should see a thank you pop-up message and you are now running an activated theme.
+
+
+![visual studio code](img/vsc.png)
